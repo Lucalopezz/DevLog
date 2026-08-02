a04d192527006896f0626dccf297aa29a51c9414
Lucalopezz <lucaslops12@gmail.com>
2026-08-01 21:43:44 -0300

    feat(api): add user controller schematic

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { InfrastructureModule } from './user/infrastructure/user.module';
 
 @Module({
-  imports: [EnvConfigModule, DatabaseModule],
+  imports: [EnvConfigModule, DatabaseModule, InfrastructureModule],
   controllers: [],
   providers: [],
 })
---
+++ apps/api/src/user/infrastructure/user.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UserController {
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {}
+
+  @Get()
+  findAll() {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {}
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {}
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {}
+}
