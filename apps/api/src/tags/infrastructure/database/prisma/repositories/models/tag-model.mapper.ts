6c033f6ff9cba8bc6c1ac157510a00f3d9aac04b
Lucalopezz <lucaslops12@gmail.com>
2026-08-13 08:49:08 -0300

    feat(api): implement tag prisma repository

---
+++ apps/api/src/tags/infrastructure/database/prisma/repositories/models/tag-model.mapper.ts
@@ -0,0 +1,21 @@
+import { ValidationError } from '@/shared/domain/errors/validation-error';
+import { TagEntity } from '@/tags/domain/entities/tag.entity';
+import { Tag } from '@generated/prisma/client';
+
+export class TagModelMapper {
+  static toEntity(model: Tag): TagEntity {
+    const data = {
+      name: model.name,
+      userId: model.userId,
+      normalizedName: model.normalizedName,
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt,
+    };
+
+    try {
+      return new TagEntity(data, model.id);
+    } catch {
+      throw new ValidationError('An entity not be loaded');
+    }
+  }
+}
---
+++ apps/api/src/tags/infrastructure/database/prisma/repositories/tag-prisma.repository.ts
@@ -0,0 +1,107 @@
+import { PrismaService } from '@/shared/infrastructure/database/prisma.service';
+import { TagEntity } from '@/tags/domain/entities/tag.entity';
+import {
+  TagFilter,
+  TagRepository,
+  TagSearchParams,
+  TagSearchResult,
+} from '@/tags/domain/repositories/tag.repository';
+import { TagModelMapper } from './models/tag-model.mapper';
+import { Prisma } from '@generated/prisma/client';
+
+export class TagPrismaRepository implements TagRepository {
+  sortableFields: string[] = ['name', 'userid', 'createdAt'];
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async insert(entity: TagEntity): Promise<void> {
+    await this.prismaService.tag.create({
+      data: entity.toJSON(),
+    });
+  }
+
+  async search(props: TagSearchParams): Promise<TagSearchResult> {
+    const where = this._buildWhere(props.filter);
+    const orderBy = this._buildOrderBy(props);
+    const skip = (props.page - 1) * props.perPage;
+
+    const [total, models] = await Promise.all([
+      this.prismaService.tag.count({ where }),
+      this.prismaService.tag.findMany({
+        where,
+        orderBy,
+        skip,
+        take: props.perPage,
+      }),
+    ]);
+
+    return new TagSearchResult({
+      items: models.map((model) => TagModelMapper.toEntity(model)),
+      total,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    });
+  }
+  private _buildOrderBy(
+    props: TagSearchParams,
+  ): Prisma.TechnicalEntryOrderByWithRelationInput {
+    if (props.sort && this.sortableFields.includes(props.sort)) {
+      return { [props.sort]: props.sortDir ?? 'asc' };
+    }
+
+    return { createdAt: 'desc' };
+  }
+
+  private _buildWhere(filter: TagFilter | null): Prisma.TagWhereInput {
+    if (!filter) {
+      return {};
+    }
+
+    const where: Prisma.TagWhereInput = {};
+
+    if (filter.userId) {
+      where.userId = filter.userId;
+    }
+
+    if (filter.name) {
+      where.name = { contains: filter.name, mode: 'insensitive' };
+    }
+
+    return where;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prismaService.tag.delete({ where: { id } });
+  }
+
+  async findByNormalizedName(
+    normalizedName: string,
+    userId: string,
+  ): Promise<TagEntity | null> {
+    const model = await this.prismaService.tag.findFirst({
+      where: { normalizedName, userId },
+    });
+    return model ? TagModelMapper.toEntity(model) : null;
+  }
+
+  async findAll(): Promise<TagEntity[]> {
+    const models = await this.prismaService.tag.findMany();
+    return models.map((model) => TagModelMapper.toEntity(model));
+  }
+
+  async findById(id: string): Promise<TagEntity | null> {
+    const model = await this.prismaService.tag.findUnique({ where: { id } });
+    return model ? TagModelMapper.toEntity(model) : null;
+  }
+  async update(entity: TagEntity): Promise<void> {
+    return this.prismaService.tag
+      .update({
+        where: { id: entity.id },
+        data: entity.toJSON(),
+      })
+      .then(() => {});
+  }
+}
