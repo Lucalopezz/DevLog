2456313ef6747ee5b3209f3cfbee641a03c94ac2
Lucalopezz <lucaslops12@gmail.com>
2026-08-31 20:36:42 -0300

    feat(api): implement project resource prisma repository

---
+++ apps/api/src/project/domain/repositories/project-resource.repository.ts
@@ -0,0 +1,28 @@
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+} from '@/shared/domain/repositories/searchable.repository';
+import { ProjectResourceEntity } from '../entities/project-resource.entity';
+import { ProjectResourceType } from '../entities/project-resource-type.enum';
+
+export type ProjectResourceFilter = {
+  projectId: string;
+  label?: string;
+  url?: string;
+  type?: ProjectResourceType;
+};
+
+export class ProjectResourceSearchParams extends SearchParams<ProjectResourceFilter> {}
+
+export class ProjectResourceSearchResult extends SearchResult<
+  ProjectResourceEntity,
+  ProjectResourceFilter
+> {}
+
+export type ProjectResourceRepository = SearchableRepositoryInterface<
+  ProjectResourceEntity,
+  ProjectResourceFilter,
+  ProjectResourceSearchParams,
+  ProjectResourceSearchResult
+>;
